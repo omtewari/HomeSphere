27c6d5223e9cfe5383e6e6a7c11d1f84379daafb
Om Tewari <omtewari106@gmail.com>
2025-03-12 09:23:24 +0530

    first initialisation of project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import{BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import About from "./pages/about"
+import Header from "./components/Header"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/about" element={<About/>}/>
+     <Route path="/profile" element={<Profile/>}/>
+     <Route path="/sign-up" element={<SignUp/>}/>
+     <Route path="/sign-in" element={<SignIn/>}/>
+
+
+
+
+    </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='text-red-600'>
+      hello
+    </div>
+  )
+}
+
+export default SignUp
